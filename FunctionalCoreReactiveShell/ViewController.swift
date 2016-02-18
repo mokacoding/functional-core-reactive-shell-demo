66cf380674a70f27f2643de155d0f9fc4f0546f7
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-18 20:37:17 +1100

    Let's get started 🐣

---
+++ FunctionalCoreReactiveShell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunctionalCoreReactiveShell
+//
+//  Created by Giovanni on 18/02/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
