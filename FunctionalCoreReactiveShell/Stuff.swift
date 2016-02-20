25cef9eeb133edf7b926f2af426bbd7e21776e05
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-20 16:55:08 +1100

    Add Stuff domain model

---
+++ FunctionalCoreReactiveShell/Stuff.swift
@@ -0,0 +1,13 @@
+//
+//  Stuff.swift
+//  FunctionalCoreReactiveShell
+//
+//  Created by Giovanni on 20/02/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+struct Stuff {
+  let id: String
+  let text: String
+  let num: Int
+}
