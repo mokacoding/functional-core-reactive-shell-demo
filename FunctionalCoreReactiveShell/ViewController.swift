25c67e22aaae7d0867c0737d1c06d45ab1574dbd
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-20 19:41:10 +1100

    Add table view data source scaffold

--- FunctionalCoreReactiveShell/ViewController.swift
+++ FunctionalCoreReactiveShell/ViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
   @IBOutlet var tableView: UITableView!
 
+  var data: [Stuff] = []
+
+  let cellIdentifier = "Cell"
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    tableView.dataSource = self
+    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+  }
+
+  // MARK: UITableViewDataSource
+
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+
+    let stuff = data[indexPath.row]
+
+    cell.textLabel?.text = stuff.text
+
+    return cell
   }
 }
-
