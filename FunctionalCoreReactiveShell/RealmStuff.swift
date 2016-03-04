dd9acd9276a21143cd928fe162214303684022b9
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-03-04 15:26:35 +1100

    Add Realm model for Stuff

---
+++ FunctionalCoreReactiveShell/RealmStuff.swift
@@ -0,0 +1,17 @@
+//
+//  RealmStuff.swift
+//  FunctionalCoreReactiveShell
+//
+//  Created by Giovanni on 4/03/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmStuff: Object {
+
+  dynamic var id: String = ""
+  dynamic var text: String = ""
+  dynamic var number: Int = 0
+}
